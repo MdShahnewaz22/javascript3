5581fe922a708a30ed91e4db6a3c516e424303ce
MdShahnewaz22 <128595173+MdShahnewaz22@users.noreply.github.com>
2023-07-27 19:32:18 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//text-change
+function textChange(){
+	document.getElementById('h2').innerHTML ="there!"
+
+}
+
+function output() {
+	var number1 = 10;
+	var number2 = 5;
+	var result =number1 + number2;
+	document.getElementById('op').innerHTML =result;
+}
+
+function test() {
+	document.getElementsByClassName('p')[1].innerHTML="welcome";
+}
+
+
+function ms() {
+	let text = "Hello";
+     text += " World";
+	document.getElementById("demo").innerHTML = text;
+		}
+
+//Division Assignment
+function sm() {
+	let x = 3;
+        x /= 100000;
+ document.getElementById("d").innerHTML = x;
+}
+
+
+
+
+
+
+
+
